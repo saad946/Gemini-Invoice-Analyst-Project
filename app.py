1c278c88280bdfdf46f61eac0742ff4510c2884b
saad <saadullah946@gmail.com>
2024-10-30 19:40:16 +0500

    adding gemini invoice analyst project

---
+++ app.py
@@ -0,0 +1,75 @@
+from dotenv import load_dotenv
+from PIL import Image
+import google.generativeai as genai
+import streamlit as st
+import os
+
+# Load environment variables from .env file
+
+load_dotenv()
+
+# Read environment variable
+
+api_key = os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=api_key)
+
+## function to load Gemini Pro vision model and get respomse
+
+def get_gemini_rsponse(input,image_data,input_prompt):
+    ## loading the gemini pro vision model
+    model= genai.GenerativeModel('gemini-1.5-flash')
+    response= model.generate_content([input,image_data[0],input_prompt])
+    return response.text 
+
+
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+
+    # initialize our streamlit app
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Invoice Analyst")
+input=st.text_input("Input Prompt: ", key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+submit= st.button("Tell me about the invoice")
+
+input_prompt="""
+You are an expert in understanding invoices. You will receive input images as invoices and you will have to answer questions based on the input image.
+"""
+
+# if submit button is clicked
+if submit:
+    image_data= input_image_setup(uploaded_file)
+    response=get_gemini_rsponse(input,image_data,input_prompt)
+    
+    st.subheader("Generated Response:")
+    st.write(response)
+
+
+
+
+
+    
+
+
+
+
